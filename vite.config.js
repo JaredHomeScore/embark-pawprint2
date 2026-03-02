89b9481f58ed0ffad400af2233aac2b9bd8cb458
Jared Rosen <145681151+JaredHomeScore@users.noreply.github.com>
2026-03-02 12:18:03 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/embark-pawprint2/',
-  define: {
-    'import.meta.env.VITE_OPENAI_API_KEY': JSON.stringify(process.env.VITE_OPENAI_API_KEY || ''),
-  },
 })
